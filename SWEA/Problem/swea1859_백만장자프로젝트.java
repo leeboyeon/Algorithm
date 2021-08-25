7b9f77c599bee3d5eb89f67cd697aa6ed2f2916f
leeboyeon <qhdus101@gmail.com>
2021-08-25 18:19:38 +0900

    comomit

--- SWEA/Problem/swea1859_백만장자프로젝트.java
+++ SWEA/Problem/swea1859_백만장자프로젝트.java
@@ -1,10 +1,14 @@
 package SWEA.Problem;
 
+import java.util.Scanner;
+
 public class swea1859_백만장자프로젝트 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello ssafy");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str);
 	}
 
 }
