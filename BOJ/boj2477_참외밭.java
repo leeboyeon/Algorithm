3dfc4fb1dc3d4e8bdc9b4b399cf877e411266f55
leeboyeon <qhdus101@gmail.com>
2021-08-25 18:40:41 +0900

    commit

---
+++ BOJ/boj2477_참외밭.java
@@ -0,0 +1,7 @@
+package BOJ;
+
+public class boj2477_참외밭 {
+    public static void main(String[] args) {
+
+    }
+}
--- SWEA/Problem/swea1859_백만장자프로젝트.java
+++ SWEA/Problem/swea1859_백만장자프로젝트.java
@@ -1,14 +1,40 @@
 package SWEA.Problem;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class swea1859_백만장자프로젝트 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		System.out.println(str);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			int size = Integer.parseInt(br.readLine());
+			int[] price = new int[size];
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < size; i++) {
+				price[i] = Integer.parseInt(st.nextToken());
+			}
+			int max = 0;
+			int buy = 0;
+			int result = 0;
+			for (int i = price.length - 1; i >= 0; i--) {
+
+				if (price[i] < max) {
+					buy += max - price[i];
+					result += buy;
+				} else if (price[i] >= max) {
+					max = price[i];
+				}
+
+			}
+			System.out.println("#" + tc + " " + result);
+		}
+
 	}
 
 }
