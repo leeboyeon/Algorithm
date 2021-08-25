b786a0bbe184d1ae9991f498a9b257f0caccdd89
leeboyeon <qhdus101@gmail.com>
2021-08-25 18:16:57 +0900

    leeboyeon

---
+++ SWEA/Problem/swea1859_백만장자프로젝트.java
@@ -0,0 +1,10 @@
+package SWEA.Problem;
+
+public class swea1859_백만장자프로젝트 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello ssafy");
+	}
+
+}
