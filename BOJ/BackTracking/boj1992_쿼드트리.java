a7ee7edd679612bc4b85f9d1bad768aefb88394a
leeboyeon <qhdus101@gmail.com>
2021-09-20 22:49:44 +0900

    commit boj

--- BOJ/BackTracking/boj1992_쿼드트리.java
+++ BOJ/BackTracking/boj1992_쿼드트리.java
@@ -4,11 +4,13 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class boj1992_쿼드트리 {
     static int N;
     static int[][] map;
     static boolean[][] visit;
+    static ArrayList<String> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,30 +25,28 @@ public class boj1992_쿼드트리 {
             }
         }
         archiveFile(0, 0, N);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i));
+        }
     }
 
     public static void archiveFile(int x, int y, int size) {
 
-        boolean flag = true;
-        for (int i = x; i <= x + size; i++) {
-            for (int j = y; j <= j + size; j++) {
-                if (map[i][j] != map[x][y]) {
-                    flag = false;
+        boolean flag = false;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (map[y + i][x + j] != map[y][x]) {
+                    list.add("(");
+                    archiveFile(x, y, size / 2);
+                    archiveFile(x + size / 2, y, size / 2);
+                    archiveFile(x, y + size / 2, size / 2);
+                    archiveFile(x + size / 2, y + size / 2, size / 2);
+                    list.add(")");
+                    return;
                 }
             }
         }
 
-        if (flag) {
-            System.out.println(map[x][y]);
-        } else {
-            System.out.print("(");
-            int nextSize = size / 2;
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
-                    archiveFile(x + nextSize * i, y + nextSize * j, nextSize);
-                }
-            }
-            System.out.println(")");
-        }
+        list.add(String.valueOf(map[y][x]));
     }
 }
