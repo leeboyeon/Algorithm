a3787fecc898ce0579bc458d5f989cb593f5308d
leeboyeon <qhdus101@gmail.com>
2021-09-29 14:45:40 +0900

    comit

---
+++ BOJ/Graph/boj11581_구호물자.java
@@ -0,0 +1,28 @@
+package BOJ.Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj11581_구호물자 {
+    static int[] input;
+    static ArrayList<Integer>[] list;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
+        int N = Integer.parseInt(br.readLine());
+       
+        input = new int[N+1];
+        for(int i=1;i<N+1;i++){
+            list[i] = new ArrayList<Integer>();
+        }
+        for(int u=1;u<N;j++){
+            int 
+        }
+    }
+}
