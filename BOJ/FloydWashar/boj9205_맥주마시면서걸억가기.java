afdbd5a3cf71c591d761c3f71c7e563116831968
leeboyeon <qhdus101@gmail.com>
2021-11-09 20:17:48 +0900

    commit

--- BOJ/FloydWashar/boj9205_맥주마시면서걸억가기.java
+++ BOJ/FloydWashar/boj9205_맥주마시면서걸억가기.java
@@ -3,50 +3,77 @@ package BOJ.FloydWashar;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class boj9205_맥주마시면서걸억가기 {
+    static int N;
+    static ArrayList<Pair> list;
+    static int[][] map;
+    static int INF = 1000000000;
+
+    static class Pair {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int tc = Integer.parseInt(br.readLine());
         for (int t = 0; t < tc; t++) {
-            int store = Integer.parseInt(br.readLine());
-            int beer = 20;
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-            for (int c = 0; c < store + 2; c++) {
+
+            N = Integer.parseInt(br.readLine());
+            list = new ArrayList<>();
+            map = new int[N + 2][N + 2];
+
+            for (int i = 0; i < N + 2; i++) {
+                Arrays.fill(map[i], INF);
+            }
+
+            for (int i = 0; i < N + 2; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                list.get(x).add(y);
+
+                list.add(new Pair(x, y));
+
             }
 
-            boolean[][] visit = new boolean[store + 2][store + 2];
-            for (int i = 0; i < store + 2; i++) {
-                for (int j = 0; j < store + 2; j++) {
-                    ArrayList<Integer> cur = list.get(i);
-                    ArrayList<Integer> next = list.get(j);
+            for (int i = 0; i < N + 2; i++) {
+                for (int j = 0; j < N + 2; j++) {
+                    if (i == j)
+                        continue;
+                    Pair cur = list.get(i);
+                    Pair next = list.get(j);
 
-                    int distance = Math.abs(cur.get(0) - next.get(0)) + Math.abs(cur.get(1) - next.get(1));
-                    if (distance <= 1000)
-                        visit[i][j] = true;
+                    int distance = Math.abs(cur.x - next.x) + Math.abs(cur.y - next.y);
+                    if (distance <= 1000) {
+                        map[i][j] = 1;
+                    }
                 }
             }
-            for (int k = 0; k < store + 2; k++) {
-                for (int i = 0; i < store + 2; i++) {
-                    for (int j = 0; j < store + 2; j++) {
-                        if (visit[i][k] && visit[k][j]) {
-                            visit[i][j] = true;
+
+            for (int via = 0; via < N + 2; via++) {
+                for (int from = 0; from < N + 2; from++) {
+                    for (int to = 0; to < N + 2; to++) {
+                        if (map[from][via] + map[via][to] < map[from][to]) {
+                            map[from][to] = map[from][via] + map[via][to];
                         }
                     }
                 }
             }
-            if (visit[0][store + 1]) {
+
+            if (map[0][N + 1] > 0 && map[0][N + 1] < INF) {
                 System.out.println("happy");
             } else {
                 System.out.println("sad");
             }
-
         }
 
     }
