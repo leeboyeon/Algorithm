6d975e67e9398c1e3f1955cdbd0d8e96b8afcc03
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-01-03 17:48:06 +0900

    commit the today Algorithm dijkstra

--- BOJ/Graph/dijkstra/boj1916_최소비용구하기.java
+++ BOJ/Graph/dijkstra/boj1916_최소비용구하기.java
@@ -2,29 +2,40 @@ package BOJ.Graph.dijkstra;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class boj1916_최소비용구하기 {
     static int N, M;
     static int INF = 1000000000;
-    static int[][] map;
+    static ArrayList<Pair>[] map;
     static int[] dist;
+
+    static class Pair implements Comparable<Pair> {
+        int to;
+        int cost;
+
+        public Pair(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.cost - o.cost;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
 
-        map = new int[N + 1][N + 1];
+        map = new ArrayList[N + 1];
         dist = new int[N + 1];
         for (int i = 1; i < N + 1; i++) {
-            for (int j = 1; j < N + 1; j++) {
-                if (i == j)
-                    map[i][j] = 0;
-                else
-                    map[i][j] = INF;
-            }
+            map[i] = new ArrayList<>();
         }
         StringTokenizer st = null;
         for (int i = 0; i < M; i++) {
@@ -32,40 +43,50 @@ public class boj1916_최소비용구하기 {
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            
-            map[u][v] = map[v][u] = cost;
+            map[u].add(new Pair(v, cost));
+            // map[v].add(new Pair(u, cost));
+
         }
         st = new StringTokenizer(br.readLine(), " ");
         int sPoint = Integer.parseInt(st.nextToken());
         int ePoint = Integer.parseInt(st.nextToken());
 
         dijkstra(sPoint);
-        System.out.println(dist[ePoint] + 1);
+        // for (int i = 1; i < dist.length; i++) {
+        // System.out.println(dist[i]);
+        // }
+        System.out.println(dist[ePoint]);
     }
 
     public static void dijkstra(int start) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>();
         boolean[] visit = new boolean[N + 1];
         for (int i = 0; i < N + 1; i++) {
             dist[i] = INF;
         }
 
-        pq.offer(new int[] { 0, start });
+        pq.offer(new Pair(start, 0));
         dist[start] = 0;
 
         while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int u = cur[1];
-            if (visit[u])
-                continue;
+            Pair cur = pq.poll();
+            int go = cur.to;
 
-            visit[u] = true;
-            for (int v = 1; v < N + 1; v++) {
-                if (dist[v] > dist[u] + map[u][v]) {
-                    dist[v] = dist[u] + map[u][v];
-                    pq.offer(new int[] { dist[v], v });
+            if (visit[go]) {
+                continue;
+            } else {
+                visit[cur.to] = true;
+            }
+
+            for (Pair next : map[go]) {
+                if (!visit[next.to]) {
+                    if (dist[next.to] > dist[go] + next.cost) {
+                        dist[next.to] = dist[go] + next.cost;
+                        pq.offer(new Pair(next.to, dist[next.to]));
+                    }
                 }
             }
         }
+
     }
 }
