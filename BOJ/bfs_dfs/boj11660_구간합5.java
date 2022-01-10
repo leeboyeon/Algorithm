736c54df390864079cce8eaecf6a137e6a125bff
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-01-10 22:08:25 +0900

    edit algorithm

--- BOJ/bfs_dfs/boj11660_구간합5.java
+++ BOJ/bfs_dfs/boj11660_구간합5.java
@@ -10,17 +10,7 @@ public class boj11660_구간합5 {
     static int N;
     static int M;
     static int[][] map;
-    static Queue<Pair> q;
-
-    static class Pair {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+    static int[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,7 +20,7 @@ public class boj11660_구간합5 {
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N + 1][N + 1];
-        q = new LinkedList<>();
+        dp = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 1; j <= N; j++) {
@@ -38,36 +28,24 @@ public class boj11660_구간합5 {
             }
         }
 
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + map[i][j];
+            }
+        }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
-
-            q.offer(new Pair(x1, y1));
-
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
+            int result = 0;
+            result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
 
-            q.offer(new Pair(x2, y2));
-
-            solution();
+            System.out.println(result);
         }
 
     }
 
-    static void solution() {
-        int result = 0;
-        while (!q.isEmpty()) {
-            Pair start = q.poll();
-            Pair end = q.poll();
-
-            for (int i = start.x; i <= end.x; i++) {
-                for (int j = start.y; j <= end.y; j++) {
-                    result += map[i][j];
-                }
-            }
-        }
-
-        System.out.println(result);
-    }
 }
