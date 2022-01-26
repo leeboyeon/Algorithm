56ac6db3e733fbb6962c205768ea361020d3383c
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-01-26 20:15:56 +0900

    commit Today Algorithm

--- BOJ/DP/boj10164_격자상의경로.java
+++ BOJ/DP/boj10164_격자상의경로.java
@@ -17,39 +17,39 @@ public class boj10164_격자상의경로 {
 
         if (K == 0) {
             System.out.println(solution(N - 1, M - 1));
-            return;
         } else {
-            int y = K / N;
             int x = K % M - 1;
             if (x < 0)
                 x = M - 1;
+
+            int y = K / M;
+
             if (K % M == 0)
                 y--;
-            int route1 = solution(x, y);
-            int route2 = solution(N - x - 1, M - y - 1);
+            int route1 = solution(x, y); // 중간점까지값
+            int route2 = solution(M - x - 1, N - y - 1); // 중간점 - 도착지 값
 
             System.out.println(route1 * route2);
-            return;
         }
     }
 
     static int solution(int X, int Y) {
 
-        int[][] dp = new int[X + 1][Y + 1];
+        int[][] dp = new int[Y + 1][X + 1];
 
         for (int i = 1; i < X + 1; i++) {
-            dp[i][0] = 1;
+            dp[0][i] = 1;
         }
         for (int i = 1; i < Y + 1; i++) {
-            dp[0][i] = 1;
+            dp[i][0] = 1;
         }
 
         for (int j = 1; j < Y + 1; j++) {
             for (int i = 1; i < X + 1; i++) {
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                dp[j][i] = dp[j - 1][i] + dp[j][i - 1];
             }
         }
 
-        return dp[X][Y];
+        return dp[Y][X];
     }
 }
